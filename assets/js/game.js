c241a2c78686b484ca5811846293a114d618de65
Patrick <patasax22@yahoo.com>
2021-08-14 19:30:05 -0400

    player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("this is an alert! JavaScript is running!")
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?")
+console.log(playerName);
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+// fight();
